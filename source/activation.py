8af77dbb5986126e455cc74c89208e68eacc73d4
Enrico Stauss <enrico_sta@web.de>
2023-10-25 15:54:40 +0200

    Fix and extend activations and losses

--- source/activation.py
+++ source/activation.py
@@ -1,21 +1,51 @@
+from typing import Optional
 import numpy as np
 
 
 def relu(z: np.ndarray) -> np.ndarray:
-    zeros = np.zeros_like(z)
-    return np.maximum(zeros, z)
+    #zeros = np.zeros_like(z)
+    return np.maximum(0, z)
 
 
-def relu_backward(da: np.ndarray, z: np.ndarray) -> np.ndarray:
-    dz = da.copy()
-    dz[z <= 0] = 0
-    return dz
+def relu_backward(z: np.ndarray, forward: Optional[np.ndarray]) -> np.ndarray:
+    return np.where(z < 0, 0, 1)
 
 
 def sigmoid(z: np.ndarray) -> np.ndarray:
     return 1/(1+np.exp(-z))
 
 
-def sigmoid_backward(da: np.ndarray, z: np.ndarray) -> np.ndarray:
-    sig_z = np.sigmoid(z)
-    return da * sig_z * (1 - sig_z)
+def sigmoid_backward(z: np.ndarray, forward: Optional[np.ndarray]) -> np.ndarray:
+    return forward * (1 - forward)
+
+
+def identity(z: np.ndarray) -> np.ndarray:
+    return z
+
+
+def identity_backward(z: np.ndarray, forward: Optional[np.ndarray]) -> np.ndarray:
+    return np.ones_like(z)
+
+
+def sin(z: np.ndarray) -> np.ndarray:
+    return np.sin(z)
+
+
+def sin_backward(z: np.ndarray, forward: Optional[np.ndarray]) -> np.ndarray:
+    return np.cos(z)
+
+
+ACTIVATION_FUNCTIONS_FORWARD = {
+    "ReLu": relu,
+    "Sigmoid": sigmoid,
+    "Identity": identity,
+    "Sin": sin,
+}
+
+
+ACTIVATION_FUNCTIONS_BACKWARD = {
+    "ReLu": relu_backward,
+    "Sigmoid": sigmoid_backward,
+    "Identity": identity_backward,
+    "Sin": sin_backward,
+}
--- source/loss.py
+++ source/loss.py
@@ -5,3 +5,19 @@ def binary_cross_entropy(input: np.ndarray, target: np.ndarray) -> np.ndarray:
     def log_clamped(x):
         return np.clip(np.log(x), a_min=-100)
     return -(target * log_clamped(input) + (1-input) * log_clamped(1 - input))
+
+
+def binary_cross_entropy_backward(input: np.ndarray, target: np.ndarray) -> np.ndarray:
+    return (input - target)/(input * (1 - input))
+
+
+def mse(input: np.ndarray, target: np.ndarray) -> np.ndarray:
+    return np.mean((target - input)**2)
+
+
+def mse_backward(input: np.ndarray, target: np.ndarray) -> np.ndarray:
+    return -2 * (target - input) / target.shape[0]
+
+
+LOSSES_FORWARD = {"bce": binary_cross_entropy, "mse": mse}
+LOSSES_BACKWARD = {"bce": binary_cross_entropy_backward, "mse": mse_backward}
